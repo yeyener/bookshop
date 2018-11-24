3a437007a3e11711963282e93d4ddebbe7c584fb
yey <yemreyener@gmail.com>
2018-11-24 19:48:16 +0300

    indentation

--- ClientApp/src/app/components/writer-form/writer-form.component.ts
+++ ClientApp/src/app/components/writer-form/writer-form.component.ts
@@ -8,44 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WriterFormComponent implements OnInit {
 
-  writersList :any = [];
+  writersList: any = [];
 
-  newWriter : any = {};
+  newWriter: any = {};
 
-  constructor(private service : WriterService) { }
+  constructor(private service: WriterService) { }
 
   ngOnInit() {
     this.populateWriters();
   }
 
-  populateWriters(){
+  populateWriters() {
     this.writersList = [];
     this.newWriter = {};
     this.service.getAll().subscribe(w => this.writersList = w);
   }
 
-  updateWriter(ww){
-    var res = this.service.update(ww)
-      .subscribe(x => {this.populateWriters();});
+  updateWriter(ww) {
+    const res = this.service.update(ww)
+      .subscribe(x => {this.populateWriters(); });
   }
 
-  createWriter(ww){
-    var res = this.service.create(ww)
-      .subscribe(x => {this.populateWriters();});
+  createWriter(ww) {
+    const res = this.service.create(ww)
+      .subscribe(x => {this.populateWriters(); });
   }
 
-  deleteWriter(writerId, writerName){
-    var conf = confirm("Are you sure you want to delete writer " + writerName + "?");
-    if (conf){
-      var res = this.service.delete(writerId)
-              .subscribe(x => {this.populateWriters();});
-      
+  deleteWriter(writerId, writerName) {
+    const conf = confirm('Are you sure you want to delete writer ' + writerName + '?');
+    if (conf) {
+      const res = this.service.delete(writerId)
+              .subscribe(x => {this.populateWriters(); });
+
     }
   }
 
-  alertWriter(){
-    var a = 1;
-    //alert(this.writers2[0].Name);
+  alertWriter() {
+    const a = 1;
+    // alert(this.writers2[0].Name);
   }
 
 }
