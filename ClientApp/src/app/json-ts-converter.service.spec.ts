f373408f33523bd6f845ed97259c218eca8dc5a9
yey <yemreyener@gmail.com>
2018-12-11 07:00:58 +0300

    first test json converter

--- ClientApp/src/app/json-ts-converter.service.spec.ts
+++ ClientApp/src/app/json-ts-converter.service.spec.ts
@@ -1,3 +1,4 @@
+import { BookInstance } from './pocos/bookInstance';
 import { TestBed, inject } from '@angular/core/testing';
 
 import { JsonTsConverterService } from './json-ts-converter.service';
@@ -11,5 +12,36 @@ describe('JsonTsConverterService', () => {
 
   it('should be created', inject([JsonTsConverterService], (service: JsonTsConverterService) => {
     expect(service).toBeTruthy();
+
   }));
+
+  it('should return an object when convert is called', inject([JsonTsConverterService], (service: JsonTsConverterService) => {
+    const mockBookInstanceJson = {
+      'id': 2011,
+      'definitionId': 0,
+      'numberInStock': 0,
+      'pageNumber': 0,
+      'edition': 0,
+      'bookName': 'Algının Kapıları',
+      'writerName': 'Aldous Huxley',
+      'genres': [],
+      'price': 66,
+      'languageName': '',
+      'languageId': 2,
+      'publisherName': '',
+      'publisherId': 4,
+      'translatorId': 1,
+      'translatorName': '',
+      'photoPath': 'uploadFolder/b0deb240-fad0-4bfd-a69d-e9c10cca376f.jpg',
+      'photoFileName': ''
+    };
+
+    const convertResult = service.convert<BookInstance>(mockBookInstanceJson, BookInstance);
+
+
+    expect(convertResult).toBeDefined();
+    expect(convertResult.id).toBeGreaterThan(0);
+  }));
+
+
 });
